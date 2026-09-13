0ee1464833c94e162d63fde586f755788de97bbc
ikmalionn <168637244+Lyonworks@users.noreply.github.com>
2026-09-13 15:03:54 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import TechStack from './components/TechStack';
-import Certificates from './components/Certificates';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import CircularText from './components/CircularText';
 import Clock from './components/Clock';
 import Grainient from './components/Grainient';
-import { FaCertificate } from 'react-icons/fa';
 
 export default function App() {
   return (
@@ -20,7 +18,6 @@ export default function App() {
       <Hero />
       <About />
       <TechStack />
-      <Certificates />
       <Projects />
       <Footer />
       <CircularText text="Lyon" spinDuration={22} />
