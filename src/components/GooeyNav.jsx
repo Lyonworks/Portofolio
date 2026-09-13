bfdf35443f63dd3458535acfe95c57df313cb5e3
ikmalionn <168637244+Lyonworks@users.noreply.github.com>
2026-09-13 13:15:15 +0700

    font fix

--- src/components/GooeyNav.jsx
+++ src/components/GooeyNav.jsx
@@ -290,7 +290,7 @@ const GooeyNav = ({
                   onClick={e => handleClick(e, index)}
                   href={item.href}
                   onKeyDown={e => handleKeyDown(e, index)}
-                  className="font-mono font-bold outline-none py-[0.6em] px-[1em] inline-block"
+                  className="font-mono font-semibold tracking-wider"
                 >
                   {item.label}
                 </a>
